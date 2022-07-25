8f4cc8df5367c2b1b85d89edc7bb3e68ba3985e2
Jianlong Chen <jianlong99@gmail.com>
2022-07-25 21:04:52 +0800

    Move zipSource & unzipSource to ziputil.go

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"archive/zip"
 	"context"
 	"errors"
-	"fmt"
-	"io"
+	"github.com/minio/minio-go/v7"
 	"log"
 	"os"
 	"path"
@@ -13,28 +11,25 @@ import (
 	"strings"
 	"time"
 
-	"github.com/alexflint/go-arg"
-	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"golang.org/x/sys/windows"
-	"gopkg.in/ini.v1"
 )
 
 type GameInfo struct {
-	Name string
-	Dir string
+	Name     string
+	Dir      string
 	StatGlob string
 }
 
 var GameList = []GameInfo{
-	{`The Witcher 3`, `The Witcher 3\gamesaves`,`*.sav`},
+	{`The Witcher 3`, `The Witcher 3\gamesaves`, `*.sav`},
 	{`Skyrim`, `My Games\Skyrim\Saves`, `*.ess`},
 }
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	var args struct {
-		Path  string `arg:"-p" default:"config.ini" help:"config path"`
+		Path string `arg:"-p" default:"config.ini" help:"config path"`
 	}
 
 	arg.MustParse(&args)
@@ -81,7 +76,6 @@ func main() {
 			}
 		}
 
-
 		needUpload := len(matches) > 0
 		downloadTime := lastTime
 		downloadObjName := ""
@@ -92,7 +86,7 @@ func main() {
 				continue
 			}
 
-			objTime, err := time.Parse(time.RFC3339, strings.TrimPrefix(strings.TrimSuffix(obj.Key, ".zip"), info.Name + "/"))
+			objTime, err := time.Parse(time.RFC3339, strings.TrimPrefix(strings.TrimSuffix(obj.Key, ".zip"), info.Name+"/"))
 			if err != nil {
 				log.Printf("Failed to parse time %s\n", obj.Key)
 				continue
@@ -107,12 +101,12 @@ func main() {
 		}
 
 		log.Printf("Game: %s, needUpload: %v, downloadObject: %s\n", info.Name, needUpload, downloadObjName)
-		zipPath := filepath.Join(document, info.Name + ".zip")
+		zipPath := filepath.Join(document, info.Name+".zip")
 		if needUpload {
 			err = zipSource(p, zipPath)
 			checkError(err)
 
-			objName := path.Join(info.Name, lastTime.Format(time.RFC3339) + ".zip")
+			objName := path.Join(info.Name, lastTime.Format(time.RFC3339)+".zip")
 			_, err = s3Client.FPutObject(context.Background(), bucketName, objName, zipPath, minio.PutObjectOptions{
 				ContentType: "application/zip",
 			})
@@ -133,148 +127,15 @@ func main() {
 	}
 }
 
-func zipSource(source, destination string) error {
-	writer, err := os.Create(destination)
-	if err != nil {
-		return err
-	}
-
-	zw := zip.NewWriter(writer)
-	defer zw.Close()
-	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if info.Mode().IsRegular() {
-			return addFileToZip(zw, path, source)
-		}
-
-		return nil
-	})
-
-	return err
-}
-
-func addFileToZip(zipWriter *zip.Writer, filename string, dirname string) error {
-	fileToZip, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer fileToZip.Close()
-
-	// Get the file information
-	info, err := fileToZip.Stat()
-	if err != nil {
-		return err
-	}
-
-	header, err := zip.FileInfoHeader(info)
-	if err != nil {
-		return err
-	}
-
-	// Using FileInfoHeader() above only uses the basename of the file. If we want
-	// to preserve the folder structure we can overwrite this with the full path.
-	if len(dirname) < len(filename) && strings.HasPrefix(filename, dirname) {
-		header.Name = filename[len(dirname):]
-		if header.Name[0] == filepath.Separator {
-			header.Name = header.Name[1:]
-		}
-	} else {
-		header.Name = filepath.Base(filename)
-	}
-
-	// Change to deflate to gain better compression
-	// see http://golang.org/pkg/archive/zip/#pkg-constants
-	header.Method = zip.Deflate
-
-	writer, err := zipWriter.CreateHeader(header)
-	if err != nil {
-		return err
-	}
-	_, err = io.Copy(writer, fileToZip)
-	return err
-}
-
-func unzipSource(source, destination string) error {
-	// 1. Open the zip file
-	reader, err := zip.OpenReader(source)
-	if err != nil {
-		return err
-	}
-	defer reader.Close()
-
-	// 2. Get the absolute destination path
-	destination, err = filepath.Abs(destination)
-	if err != nil {
-		return err
-	}
-
-	// 3. Iterate over zip files inside the archive and unzip each of them
-	for _, f := range reader.File {
-		err := unzipFile(f, destination)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func unzipFile(f *zip.File, destination string) error {
-	// 4. Check if file paths are not vulnerable to Zip Slip
-	filePath := filepath.Join(destination, f.Name)
-	if !strings.HasPrefix(filePath, filepath.Clean(destination)+string(os.PathSeparator)) {
-		return fmt.Errorf("invalid file path: %s", filePath)
-	}
-
-	// 5. Create directory tree
-	if f.FileInfo().IsDir() {
-		if err := os.MkdirAll(filePath, os.ModePerm); err != nil {
-			return err
-		}
-		return nil
-	}
-
-	if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
-		return err
-	}
-
-	// 6. Create a destination file for unzipped content
-	destinationFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-	if err != nil {
-		return err
-	}
-	defer destinationFile.Close()
-
-	// 7. Unzip the content of a file and copy it to the destination file
-	zippedFile, err := f.Open()
-	if err != nil {
-		return err
-	}
-	defer zippedFile.Close()
-
-	if _, err := io.Copy(destinationFile, zippedFile); err != nil {
-		return err
-	}
-
-	if err := destinationFile.Close(); err != nil {
-		return err
-	}
-
-	if err := os.Chtimes(filePath, time.Now(), f.FileInfo().ModTime()); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // exists returns whether the given file or directory exists
 func isDir(path string) (bool, error) {
 	st, err := os.Stat(path)
-	if err == nil { return st.IsDir(), nil }
-	if os.IsNotExist(err) { return false, nil }
+	if err == nil {
+		return st.IsDir(), nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
 	return false, err
 }
 
---
+++ ziputil.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+func zipSource(source, destination string) error {
+	writer, err := os.Create(destination)
+	if err != nil {
+		return err
+	}
+
+	zw := zip.NewWriter(writer)
+	defer zw.Close()
+	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.Mode().IsRegular() {
+			return addFileToZip(zw, path, source)
+		}
+
+		return nil
+	})
+
+	return err
+}
+
+func addFileToZip(zipWriter *zip.Writer, filename string, dirname string) error {
+	fileToZip, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer fileToZip.Close()
+
+	// Get the file information
+	info, err := fileToZip.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+
+	// Using FileInfoHeader() above only uses the basename of the file. If we want
+	// to preserve the folder structure we can overwrite this with the full path.
+	if len(dirname) < len(filename) && strings.HasPrefix(filename, dirname) {
+		header.Name = filename[len(dirname):]
+		if header.Name[0] == filepath.Separator {
+			header.Name = header.Name[1:]
+		}
+	} else {
+		header.Name = filepath.Base(filename)
+	}
+
+	// Change to deflate to gain better compression
+	// see http://golang.org/pkg/archive/zip/#pkg-constants
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(writer, fileToZip)
+	return err
+}
+
+func unzipSource(source, destination string) error {
+	// 1. Open the zip file
+	reader, err := zip.OpenReader(source)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	// 2. Get the absolute destination path
+	destination, err = filepath.Abs(destination)
+	if err != nil {
+		return err
+	}
+
+	// 3. Iterate over zip files inside the archive and unzip each of them
+	for _, f := range reader.File {
+		err := unzipFile(f, destination)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func unzipFile(f *zip.File, destination string) error {
+	// 4. Check if file paths are not vulnerable to Zip Slip
+	filePath := filepath.Join(destination, f.Name)
+	if !strings.HasPrefix(filePath, filepath.Clean(destination)+string(os.PathSeparator)) {
+		return fmt.Errorf("invalid file path: %s", filePath)
+	}
+
+	// 5. Create directory tree
+	if f.FileInfo().IsDir() {
+		if err := os.MkdirAll(filePath, os.ModePerm); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+		return err
+	}
+
+	// 6. Create a destination file for unzipped content
+	destinationFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+
+	// 7. Unzip the content of a file and copy it to the destination file
+	zippedFile, err := f.Open()
+	if err != nil {
+		return err
+	}
+	defer zippedFile.Close()
+
+	if _, err := io.Copy(destinationFile, zippedFile); err != nil {
+		return err
+	}
+
+	if err := destinationFile.Close(); err != nil {
+		return err
+	}
+
+	if err := os.Chtimes(filePath, time.Now(), f.FileInfo().ModTime()); err != nil {
+		return err
+	}
+
+	return nil
+}
