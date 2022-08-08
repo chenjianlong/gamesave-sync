01051b4630e986b5e63220be468435a460016b6f
Jianlong Chen <jianlong99@gmail.com>
2022-08-08 23:40:55 +0800

    Add ftptransfer.go

---
+++ ftptransfer.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"github.com/jlaffaye/ftp"
+	"io"
+	"os"
+	"path"
+	"strings"
+	"time"
+)
+
+type FTPTransfer struct {
+	conn     *ftp.ServerConn
+	subDir string
+}
+
+func NewFTPTransfer(addr, user, password, subDir string) (Transfer, error) {
+	conn, err := ftp.Dial(addr, ftp.DialWithTimeout(5*time.Second))
+	if err != nil {
+		return nil, err
+	}
+
+	if err = conn.Login(user, password); err != nil {
+		return nil, err
+	}
+
+	transfer := new(FTPTransfer)
+	transfer.conn = conn
+	transfer.subDir = subDir
+	return transfer, nil
+}
+
+func (t *FTPTransfer) upload(localFile, remoteFile string) error {
+	fs, err := os.Open(localFile)
+	if err != nil {
+		return err
+	}
+
+	remoteFile = path.Join(t.subDir, remoteFile)
+	err = t.conn.Stor(remoteFile, fs)
+	if err != nil && err.Error() == "550 Couldn't open the file or directory" {
+		elements := strings.Split(remoteFile, "/")
+		for i := 1; i < len(elements); i += 1 {
+			remoteDir := path.Join(elements[:i]...)
+			if err = t.conn.MakeDir(remoteDir); err != nil {
+				return err
+			}
+		}
+
+		err = t.conn.Stor(remoteFile, fs)
+	}
+
+	return err
+}
+
+func (t *FTPTransfer) download(remoteFile, localFile string) error {
+	fs, err := os.Create(localFile)
+	if err != nil {
+		return err
+	}
+
+	remoteFile = path.Join(t.subDir, remoteFile)
+	resp, err := t.conn.Retr(remoteFile)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Close()
+	_, err = io.Copy(fs, resp)
+	return err
+}
+
+func (t *FTPTransfer) listFile(dir string) chan string {
+	resultCh := make(chan string)
+	go func() {
+		dir = path.Join(t.subDir, dir)
+		entries, err := t.conn.List(dir)
+		if err != nil {
+			close(resultCh)
+			return
+		}
+
+		for _, entry := range entries {
+			if entry.Type != ftp.EntryTypeFile {
+				continue
+			}
+
+			resultCh <- entry.Name
+		}
+		close(resultCh)
+	}()
+	return resultCh
+}
